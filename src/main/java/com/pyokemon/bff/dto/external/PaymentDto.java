68810f972cdc4186472f709a52cfd40c45b2a2ce
Nebulaesthetic <163494134+Nebulaesthetic@users.noreply.github.com>
2025-08-09 11:22:47 +0900

    Payment Dto Refactoring #3

--- src/main/java/com/pyokemon/bff/dto/external/PaymentDto.java
+++ src/main/java/com/pyokemon/bff/dto/external/PaymentDto.java
@@ -1,20 +1,21 @@
 package com.pyokemon.bff.dto.external;
 
+import com.pyokemon.bff.dto.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PaymentDto {
     private Long id;
-    private Long bookingId;
     private Integer amount;
-    private String method;  // CREDIT_CARD, BANK_TRANSFER, MOBILE
-    private String status;  // PENDING, PAID, FAILED, REFUNDED
-    private LocalDateTime paidAt;
-    private LocalDateTime createdAt;
+    private String method;
+    private PaymentStatus status;
+    private LocalDateTime updatedAt;
 }
