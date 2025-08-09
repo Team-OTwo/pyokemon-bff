b22e61ceb20f5bbaef971d2e24a4672d98cde165
Nebulaesthetic <163494134+Nebulaesthetic@users.noreply.github.com>
2025-08-09 11:23:04 +0900

    SeatPrice Dto Refactoring #3

--- src/main/java/com/pyokemon/bff/dto/external/SeatPriceDto.java
+++ src/main/java/com/pyokemon/bff/dto/external/SeatPriceDto.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class PriceDto {
+public class SeatPriceDto {
     private Long id;
-    private Long eventScheduleId;
-    private Long seatClassId;
     private Integer price;
 }
