64df67d637a84ab00c28978f860e9bec6926cee2
sky121016 <sky121016@naver.com>
2025-08-13 15:37:41 +0900

    [Feature/#5] MyPageBookingController 작성

--- src/main/java/com/pyokemon/bff/controller/MyPageBookingController.java
+++ src/main/java/com/pyokemon/bff/controller/MyPageBookingController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/mypage/bookings")
+@RequestMapping("/api/mypage/bookings")
 @RequiredArgsConstructor
 public class MyPageBookingController {
 
@@ -18,11 +20,16 @@ public class MyPageBookingController {
 
     /**
      * 사용자 마이페이지 예매 내역 조회
-     * @param accountId 사용자 ID
+     *
+//     * @param accountId 사용자 ID
      * @return 예매 목록
      */
     @GetMapping
-    public Flux<MyPageBookingResponse> getMyBookings(@RequestParam Long accountId) {
-        return myPageBookingService.getMyBookings(accountId);
+    public Flux<MyPageBookingResponse> getMyBookings(ServerWebExchange exchange) {
+        System.out.println(exchange.getRequest().getHeaders().getFirst("x-auth-accountId"));
+        return Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("x-auth-accountId"))
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Missing x-auth-accountId header")))
+                .map(Long::valueOf)
+                .flatMapMany(myPageBookingService::getMyBookings);
     }
 }
